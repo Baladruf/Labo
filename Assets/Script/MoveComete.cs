c9c92f4d67c0121d435c13cb03cf8f713d75b46b
Unknown <e.escher@rubika-edu.com>
2019-01-11 09:26:44 +0100

    Init

---
+++ Assets/Script/MoveComete.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveComete : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+    private Vector3 posDepart;
+    private bool isPlaying = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        posDepart = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPlaying = !isPlaying;
+        }else if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = posDepart;
+        }
+
+        if (isPlaying)
+        {
+            transform.position += -transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Script/RotatePlanete.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanete : MonoBehaviour
+{
+    [SerializeField] float speed = 8;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
